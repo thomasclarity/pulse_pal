a01625a89ea449bf464eea0751e6e0ce76258d81
m <mmyros@gmail.com>
2020-10-08 15:49:08 -0400

    forget poetry

--- docs/usage.rst
+++
@@ -1,7 +0,0 @@
-=====
-Usage
-=====
-
-To use Pulse Pal for Python 3 in a project::
-
-    import pulse_pal
--- pulse_pal/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Top-level package for Pulse Pal for Python 3."""
-
-__author__ = """Maxym Myroshnychenko"""
-__email__ = 'mmyros@gmail.com'
-__version__ = '0.1.0'
